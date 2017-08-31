3cd8a297633c3996cb2af2071ddc546eb8ced993
Santhosh Nageshwar <santhoshbabun@gmail.com>
2017-08-30 23:11:25 -0700

    Adding the api requests

---
+++ src/api/index.js
@@ -0,0 +1,86 @@
+const api = "https://reactnd-books-api.udacity.com"
+
+
+// Generate a unique token for storing your bookshelf data on the backend server.
+let token = localStorage.token
+if (!token)
+  token = localStorage.token = Math.random().toString(36).substr(-8)
+
+const headers = {
+  'Accept': 'application/json',
+  'Authorization': token
+}
+
+export const getAllPosts = () =>
+  fetch(`${api}/posts`, { headers })
+    .then(res => res.json())
+    .then(data => data)
+
+export const getCommentsForPost = (postId) =>
+  fetch(`${api}/posts/${postId}/comments`, { headers })
+    .then(res => res.json())
+    .then(data => data)
+
+export const getCategories = () =>
+  fetch(`${api}/categories`, { headers })
+    .then(res => res.json())
+    .then(data => data.categories)
+
+export const getAllPostsInCategory = (category) =>
+  fetch(`${api}/${category}/posts`, { headers })
+    .then(res => res.json())
+    .then(data => data)
+
+export const addNewPost = (title, body, author, category) =>
+  fetch(`${api}/posts`, {
+    method: 'POST',
+    headers: headers,
+    body: JSON.stringify({ title, body, author, category, id: Math.random().toString(24), timestamp:Date.now()})
+  }).then(res => res.json())
+
+export const editPost = (postId, title, body) =>
+  fetch(`${api}/posts/${postId}`, {
+    method: 'PUT',
+    headers: headers,
+    body: JSON.stringify({ title, body})
+  }).then(res => res.json())
+
+export const deletePost = (postId) =>
+  fetch(`${api}/posts/${postId}`, {
+    method: 'DELETE',
+    headers: headers
+  }).then(res => res.json())
+
+export const voteOnPost = (postId, thumbsUp) =>
+  fetch(`${api}/posts/${postId}`, {
+    method: 'POST',
+    headers: headers,
+    body: JSON.stringify({ option: thumbsUp? 'upVote' : 'downVote'})
+  }).then(res => res.json())
+
+export const addNewComment = (body, author, parentId) =>
+  fetch(`${api}/comments`, {
+    method: 'POST',
+    headers: headers,
+    body: JSON.stringify({ parentId, body, author, id: (parentId + Math.random().toString(12)), timestamp:Date.now()})
+  }).then(res => res.json())
+
+export const editComment = (commentId, body) =>
+  fetch(`${api}/comments/${commentId}`, {
+    method: 'PUT',
+    headers: headers,
+    body: JSON.stringify({timestamp:Date.now(), body})
+  }).then(res => res.json())
+
+export const deleteComment = (commentId) =>
+  fetch(`${api}/comments/${commentId}`, {
+    method: 'DELETE',
+    headers: headers
+  }).then(res => res.json())
+
+export const voteOnComment = (commentId, thumbsUp) =>
+  fetch(`${api}/comments/${commentId}`, {
+    method: 'POST',
+    headers: headers,
+    body: JSON.stringify({ option: thumbsUp? 'upVote' : 'downVote'})
+  }).then(res => res.json())
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { createStore, combineReducers } from 'redux'
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk';
 import { Provider } from 'react-redux'
 import { Router, Route, browserHistory } from 'react-router'
 import { syncHistoryWithStore, routerReducer } from 'react-router-redux'
@@ -11,12 +12,14 @@ import CreatePost from './components/CreatePost'
 import ViewPost from './components/ViewPost'
 import registerServiceWorker from './registerServiceWorker';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
 // Add the reducer to your store on the `routing` key
 const store = createStore(
   combineReducers({
     ...reducers,
     routing: routerReducer
-  })
+  }), composeEnhancers(applyMiddleware(thunk))
 )
 
 // Create an enhanced history that syncs navigation events with the store
@@ -24,12 +27,13 @@ const history = syncHistoryWithStore(browserHistory, store)
 
 ReactDOM.render(
   <Provider store={store}>
-  <Router history={history}>
-    <div>
-      <Route exact path="/" component={App}/>
-      <Route exact path="/post/:postId" component={ViewPost}/>
-      <Route exact path="/create" component={CreatePost}/>
-    </div>
-  </Router>
-</Provider>, document.getElementById('root'));
+    <Router history={history}>
+      <div>
+        <Route exact path="/" component={App}/>
+        <Route exact path="/post/:postId" component={ViewPost}/>
+        <Route exact path="/create" component={CreatePost}/>
+      </div>
+    </Router>
+  </Provider>,
+  document.getElementById('root'));
 registerServiceWorker();
