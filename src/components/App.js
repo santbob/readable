1bec9be16d9cd8ab3430c6b318fc1f1abe8a6936
Santhosh Nageshwar <santhoshbabun@gmail.com>
2017-09-05 23:11:52 -0700

    Rearranging layouts to accomodate whole design

--- src/components/App.js
+++ src/components/App.js
@@ -14,21 +14,20 @@ class App extends Component {
   render() {
     const {categories, posts} = this.props
     return (
-      <div  id="layout" className="pure-g">
-        <div className="sidebar pure-u-1 pure-u-md-1-4">
-          <div className="header">
-            <h1 className="brand-title">The Reader</h1>
-            <h2 className="brand-tagline">A React Nanodegree project</h2>
-            <nav className="nav">
-                <ul className="nav-list">
-                  {categories && categories.map((category) => (
-                    <li className="nav-item" key={category.path}><Link to={`${category.path}`} className="pure-button"><b>{category.name}</b></Link></li>
-                  ))}
-                </ul>
-            </nav>
-          </div>
+      <div id="layout">
+        <div className="header">
+          <h1 className="brand-title">The Reader</h1>
+          <h2 className="brand-tagline">A React Nanodegree project</h2>
+          <Link to="/create"><button className="top_right fab_btn">+</button></Link>
+          <nav className="nav">
+              <ul className="nav-list">
+                {categories && categories.map((category) => (
+                  <li className="nav-item" key={category.path}><Link to={`${category.path}`} className="pure-button"><b>{category.name}</b></Link></li>
+                ))}
+              </ul>
+          </nav>
         </div>
-        <div className="content pure-u-1 pure-u-md-3-4">
+        <div className="content">
           <ListPosts />
         </div>
       </div>
